993a9abc87012138ea28dd57020db3702696728a
gskamau <gskamau@users.noreply.github.com>
2017-05-15 13:17:56 -0700

    Minor edit

--- album.php
+++ album.php
@@ -1,30 +1,66 @@
 #!/usr/local/php5/bin/php-cgi
-<?php include 'include/config.php';
+<?php include 'config.php';
 $conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
 $error = mysqli_connect_error();
 if ($error != null) {
   $output = "<p>Unable to connect to database</p>" . $error;
   exit($output);
 } ?>
-<?php include 'include/header.php' ?>
-
-<main>
- <h1><?php echo $_GET["album"] . " " . $_GET["year"]; ?></h1>
-    <?php
-       global $conn;
-       $sql = "SELECT Url FROM Photos WHERE Album = '". $_GET["album"] . "';";
-       $result = mysqli_query($conn, $sql);
-       if($result=mysqli_query($conn, $sql)) {
-          while($rows=mysqli_fetch_assoc($result)) { ?>
-             <img class="gallery" src="<?php echo $rows["Url"]; ?>" alt="">
-          <?php
-          }
-       mysqli_free_result($result);
-       } else { echo "no result<br>";}
-       mysqli_close($conn);
-       ?>
-
-	<div class="clear"></div>
-</main>
-
- <?php include 'include/footer.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo $_GET["Album"] . " " . $_GET["Year"]; ?></title>
+		<link rel="stylesheet" type="text/css" href="project.css">
+	</head>
+	<p>Student Project - not a commercial site.</p>	
+<?php include 'header.php' ?>
+	
+	<main>
+   <h1><?php echo $_GET["Album"] . " " . $_GET["Year"]; ?></h1>
+   <!--Wrap everything in parent div to get images for modals-->
+   <div id="parent_modal">
+      <?php 
+         global $conn;
+         $sql = "SELECT Url FROM Photos WHERE Album = '". $_GET["Album"] . "';";
+         $result = mysqli_query($conn, $sql);
+         if($result=mysqli_query($conn, $sql)) {
+            while($rows=mysqli_fetch_assoc($result)) { ?>
+               <img class="gallery" src="<?php echo $rows["Url"]; ?>" alt="Photo from the album <?php echo $rows["Album"]; ?>">
+				<?php				
+            }
+         mysqli_free_result($result);
+         } else { echo "no result<br>";}
+         mysqli_close($conn);
+         ?>
+	</div>
+	<div class="clear"></div>	
+		<!-- The Modal -->
+		<div id="myModal" class="modal">
+		  <span class="close">&times;</span>
+		  <img class="modal-content" id="img01" src="">
+		  <div id="caption"></div>
+		</div>		
+	</main>
+	
+	<script>
+	var modal = document.getElementById('myModal');
+	var img = document.getElementById('parent_modal').childNodes;
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+	for(var i=0; i<img.length; i++) {
+		img[i].onclick = function() {
+		 modal.style.display = "block";
+		 modalImg.src = this.src;
+		 captionText.innerHTML = this.alt;
+		}
+	}
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() { 
+	modal.style.display = "none";
+	}
+	</script>
+   <?php include 'footer.php' ?>
+</html>
