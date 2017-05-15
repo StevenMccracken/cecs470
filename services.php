d88400e8baf3df27ec0dbd462fb2682bd49024cc
gskamau <gskamau@users.noreply.github.com>
2017-05-15 12:31:55 -0700

    Minor updates to services.php

--- services.php
+++ services.php
@@ -22,20 +22,20 @@ if ($error != null) {
       <?php 
       $i = 0; //counter for left/right class logic
       global $conn;
-      $sql = "SELECT Type, ThumbnailUrl FROM Services;";
+      $sql = "SELECT ID, Type, ThumbnailUrl FROM Services;";
          $result = mysqli_query($conn, $sql);
          if($result=mysqli_query($conn, $sql)) {
-            while($rows=mysqli_fetch_assoc($result)) { ?>
+            while($sRows=mysqli_fetch_assoc($result)) { ?>
          <article>
-            <img class="<?php if($i%2 == 0) echo "left"; else echo "right"; ?>" src="<?php echo $rows["ThumbnailUrl"]; ?>" alt="<?php echo $rows["Type"]; ?> Services" />
-            <h2 class="<?php if($i%2 == 0) echo "right"; else echo "left"; ?>"><?php echo strtoupper($rows["Type"]); ?></h2>
+            <img class="<?php if($i%2 == 0) echo "left"; else echo "right"; ?>" src="<?php echo $sRows["ThumbnailUrl"]; ?>" alt="<?php echo $sRows["Type"]; ?> Services" />
+            <h2 class="<?php if($i%2 == 0) echo "right"; else echo "left"; ?>"><?php echo strtoupper($sRows["Type"]); ?></h2>
             <p class="<?php if($i%2 == 0) echo "right"; else echo "left"; ?>">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet lacus sit amet libero facilisis pulvinar. Cras fermentum commodo suscipit.
             </p>
             
             <?php //!
             global $conn;
-            $pSql = "SELECT Name, Price, Locations, Outfits, Duration FROM Packages WHERE ServiceType = '" .$rows["Type"]."' ORDER BY Duration;";
+            $pSql = "SELECT Name, Price, Locations, Outfits, Duration FROM Packages WHERE Service = '".$sRows["ID"]."' ORDER BY Duration;";
             $pResult = mysqli_query($conn, $pSql);
             if($pResult=mysqli_query($conn, $pSql)) {
                while($pRows=mysqli_fetch_assoc($pResult)) { ?>
