25df2e1be93e948b7513f4d5b0f9ea48ef8955d3
chelseasoldan <csoldan@live.com>
2017-05-15 21:03:13 -0700

    Update header with logo
    Minor change to project.css and header.php to add logo to header

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title><?php if (isset($title)) echo $title; else echo "PHILIP NGUYEN"; ?></title>
+		<link rel="stylesheet" type="text/css" href="<?php echo $path; ?>reset.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo $path; ?>project.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo $path; ?>admin.css">
+	</head>
+  <body>
+  	<p>Student Project - not a commercial site.</p>
+
+    <header>
+     <img class="logo" src="logo.jpg" alt="Philip Nguyen Photography">
+        <nav>
+           <a href="<?php echo $path; ?>index.php">HOME</a>
+           <a href="<?php echo $path; ?>services.php">SERVICES</a>
+           <a href="<?php echo $path; ?>book.php">BOOK</a>
+           <a href="<?php echo $path; ?>aboutphilip.php">ABOUT</a>
+        </nav>
+     <div class="clear"></div>
+   </header>
